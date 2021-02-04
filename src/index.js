169a52d87e4d8ad7b9c21b8f603a31dfa7f5a11f
lijiahong <1471363285@qq.com>
2021-02-04 23:21:09 +0800

    添加了js处理,eslint和语法转换,高级语法转换为低级语法

--- src/index.js
+++ src/index.js
@@ -6,7 +6,30 @@ import './style/b.scss';
 // import "./style/index.scss";
 console.log('index.js文件加载了');
 
-function count(a, b) {
-  return a + b;
-}
+const count = (a, b) => a + b;
 console.log(count(2, 5));
+
+const promise = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('定时器执行了~');
+    resolve();
+  }, 1000);
+});
+
+console.log(promise, 123);
+// @perple
+// class Add {
+//   constructor(name) {
+//     this.name = name
+//   }
+//   eat() {
+//     console.log(this.name + '在吃东西,他有' + this.age + '岁了',);
+//   }
+// }
+
+// const add = new Add('刘德华')
+// add.eat()
+
+// function perple(target) {
+//   targrt.prototype.age = 18
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ const cssLoader = [
 
 
 module.exports = {
-    mode: 'development', // 模式 production development
+    mode: 'production', // 模式 production development  注意production模式下,会自动压缩js
     entry: './src/index.js', // 入口文件
     output: {
         filename: 'js/bandle.js', // 输出文件的文件名
@@ -88,15 +88,50 @@ module.exports = {
             注意:只检查自己写的源代码,引用的代码不做检查
             设置检查规则: 用 airbnb 库的语法规范来做检查 --> eslint-config-airbnb-base  eslint-plugin-import  eslint
             然后添加 "extends": "airbnb-base/legacy" 到 .eslintrc 文件内
-
+            
+            js语法转换 Es6+ --> ES5-
+            需要下载 ==>   babel-loader @babel/core @babel/preset-env 
+                1. 基本js兼容性问题 --> @babel/preset-env
+                    问题:只能转换基本语法,如promise不能转换
+                2. 全部js兼容性处理 --> @babel/polyfill ,用法:在需要考虑兼容性的页面直接 import "@babel/polyfill"
+                    问题: 我们只需要解决部分兼容性问题,但是引入了所有兼容性代码,体积太大了
+                3.按需加载兼容性处理代码: --> core-js
             */
             {
                 test: /\.js$/,
                 exclude: /node_modules/, // 排除node_miodules
-                loader: 'eslint-loader',
-                options: {
-                    fix: true // 打开自动修复
-                }
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [ // 预设: 指示babel做怎样的兼容性处理
+                                ['@babel/preset-env',
+                                    // core-js
+                                    {
+                                        useBuiltIns: 'usage', // 按需加载
+                                        // 指定core-js版本
+                                        corejs: {
+                                            version: 3
+                                        },
+                                        // 指定兼容性做到哪个版本的浏览器
+                                        targets: {
+                                            chrome: '60',
+                                            firefox: '60',
+                                            ie: '6',
+                                            safari: '10',
+                                            edge: '17'
+                                        }
+                                    }]
+                            ]
+                        }
+                    },
+                    {
+                        loader: 'eslint-loader',
+                        options: {
+                            fix: true // 打开自动修复
+                        }
+                    }
+                ]
             },
             // 处理css
             {
